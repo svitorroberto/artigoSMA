a9d672c81d1ae4de313fd66ba8ef537dd06552ac
Vítor Roberto <vitorroberto.info@gmail.com>
2017-06-07 23:51:16 -0300

    Interface REST

---
+++ src/main/java/ufg/grupo3/Entidade/Agente.java
@@ -0,0 +1,23 @@
+package ufg.grupo3.Entidade;
+
+public class Agente {
+	
+	private String id;
+	
+	public Agente() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Agente(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
