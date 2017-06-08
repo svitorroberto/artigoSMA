335679a7f74b1ca11c4eabd2f1b8a5a07eb55806
Vítor Roberto <vitorroberto.info@gmail.com>
2017-06-07 23:14:50 -0300

    STARTUP

---
+++ src/main/java/ufg/grupo3/api_jade_rest/App.java
@@ -0,0 +1,13 @@
+package ufg.grupo3.api_jade_rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
